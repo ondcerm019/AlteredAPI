66abdb66d5963d5188493690d259d1a314260b6a
ondrej_cermak <cerm.ondrej@gmail.com>
2022-05-15 20:02:24 +0200

    first alteration

---
+++ DrinksAPI/Controllers/RootController.cs
@@ -0,0 +1,33 @@
+﻿using DrinksAPI.Data;
+using DrinksAPI.Models;
+using DrinksAPI.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DrinksAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RootController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public RootController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<RootObject>>> GetDrinks()
+        {
+            return Ok(new RootObject {
+                Drinks = await _context.Drinks.Include(d => d.Restaurants)
+                .ThenInclude(d => d.Restaurant)
+                .Select(d => new RDrink { Id = d.Id, Name = d.Name, Restaurants = d.Restaurants.Select(r => new RelObj { Id = r.RestaurantId, Cost = r.Cost }).ToList() }).ToListAsync(),
+                Restaurants = await _context.Restaurants.Include(r => r.Drinks).ThenInclude(r => r.Drink).Select(r => new RRestaurant { Id = r.Id, Name = r.Name, Drinks = r.Drinks.Select(d => new RelObj { Id = d.DrinkId, Cost = d.Cost }).ToList() }).ToListAsync()
+                });
+        }
+    }
+}
+
---
+++ DrinksAPI/Models/RootObject.cs
@@ -0,0 +1,30 @@
+﻿using DrinksAPI.ViewModels;
+
+namespace DrinksAPI.Models
+{
+    public class RootObject
+    {
+        public IList<RDrink>? Drinks { get; set; }
+        public IList<RRestaurant>? Restaurants { get; set; }
+    }
+
+    public class RDrink
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public IList<RelObj>? Restaurants { get; set; }
+    }
+
+    public class RRestaurant
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public IList<RelObj>? Drinks { get; set; }
+    }
+
+    public class RelObj
+    {
+        public int Id { get; set; }
+        public double Cost { get; set; }
+    }
+}
